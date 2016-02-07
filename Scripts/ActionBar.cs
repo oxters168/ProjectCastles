9ac66644d1ea356f64141563f5f79c7a6dec1e7a
oxters168 <oxters168@gmail.com>
2016-02-08 00:22:48 +0400

    Added extra files for previous commit

---
+++ Scripts/ActionBar.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionBar : OxGUI3D {
+
+    public float percentComplete = 1f;
+    public bool centeredBar = false;
+    public bool flip = false;
+    private bool flipping = false;
+    public float degrees = 180f, seconds = 0.5f, fps = 30f;
+
+    public Color mainColor = Color.blue, backgroundColor = Color.gray;
+    private Material borderMaterial, fillMaterial;
+
+    private Vector3 originalLoadingBarPosition, originalLoadingBarScale;
+    private Transform loadingBar;
+
+	// Use this for initialization
+	protected override void Start ()
+    {
+        base.Start();
+
+        #region Initialise Materials
+        borderMaterial = new Material(Shader.Find("Legacy Shaders/Diffuse"));
+        fillMaterial = new Material(Shader.Find("Legacy Shaders/Diffuse"));
+        UpdateColors();
+        #endregion
+
+        #region Initial Loading Bar Setup
+        loadingBar = transform.FindChild("FillingTopHalf").FindChild("LoadingBar");
+        originalLoadingBarPosition = loadingBar.localPosition;
+        originalLoadingBarScale = loadingBar.localScale;
+        #endregion
+
+        #region Set Materials unto ActionBar
+        loadingBar.GetComponent<Renderer>().material = borderMaterial;
+        transform.FindChild("FillingBottomHalf").GetComponent<Renderer>().material = fillMaterial;
+        transform.FindChild("BorderTop").GetComponent<Renderer>().material = borderMaterial;
+        transform.FindChild("BorderBottom").GetComponent<Renderer>().material = borderMaterial;
+        transform.FindChild("BorderLeft").GetComponent<Renderer>().material = borderMaterial;
+        transform.FindChild("BorderRight").GetComponent<Renderer>().material = borderMaterial;
+        transform.FindChild("FillingTopHalf").FindChild("Top").GetComponent<Renderer>().material = fillMaterial;
+        transform.FindChild("FillingTopHalf").FindChild("Bottom").GetComponent<Renderer>().material = fillMaterial;
+        transform.FindChild("FillingTopHalf").FindChild("Left").GetComponent<Renderer>().material = fillMaterial;
+        transform.FindChild("FillingTopHalf").FindChild("Right").GetComponent<Renderer>().material = fillMaterial;
+        #endregion
+    }
+	
+	// Update is called once per frame
+	protected override void Update ()
+    {
+        base.Update();
+        UpdateColors();
+        UpdateLoadingBar();
+        if (flip)
+        {
+            flip = false;
+            FlipBar(degrees, seconds, fps);
+        }
+	}
+
+    private void UpdateColors()
+    {
+        borderMaterial.color = mainColor;
+        fillMaterial.color = backgroundColor;
+    }
+    private void UpdateLoadingBar()
+    {
+        if (percentComplete > 1) percentComplete = 1f;
+        else if (percentComplete < 0) percentComplete = 0f;
+
+        loadingBar.localScale = new Vector3(originalLoadingBarScale.x * percentComplete, originalLoadingBarScale.y, originalLoadingBarScale.z);
+
+        if (centeredBar)
+        {
+            loadingBar.localPosition = originalLoadingBarPosition;
+        }
+        else
+        {
+            loadingBar.localPosition = originalLoadingBarPosition + new Vector3(loadingBar.localScale.x / 2f, 0, 0) - new Vector3(originalLoadingBarScale.x / 2f, 0, 0);
+        }
+    }
+
+    public void FlipBar(float degrees, float seconds, float fps)
+    {
+        if (!flipping)
+        {
+            StartCoroutine(Flip(degrees, seconds, fps));
+        }
+    }
+    private IEnumerator Flip(float degrees, float seconds, float fps)
+    {
+        flipping = true;
+
+        float countDownDegrees = degrees, splitDegrees = degrees / (fps * seconds), splitSeconds = 1 / fps, currentTime = Time.time;
+        Quaternion originalRotation = transform.localRotation;
+
+        while (true)
+        {
+            //yield return new WaitForSeconds(splitSeconds);
+            if (Time.time - currentTime >= splitSeconds)
+            {
+                transform.Rotate(new Vector3(splitDegrees, 0, 0));
+                countDownDegrees -= splitDegrees;
+                if (countDownDegrees <= 0) { transform.localRotation = originalRotation * Quaternion.AngleAxis(degrees, Vector3.right); break; }
+                currentTime = Time.time;
+            }
+
+            yield return null;
+        }
+
+        flipping = false;
+    }
+}
---
+++ Scripts/OxGUI3D.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OxGUI3D : MonoBehaviour {
+
+    private GameObject movementHelper;
+    private Vector3 cameraDifference, originalScale;
+
+	// Use this for initialization
+	protected virtual void Start ()
+    {
+        movementHelper = new GameObject("MovementHelp");
+        movementHelper.transform.position = transform.position;
+        movementHelper.transform.forward = Camera.main.transform.up;
+        transform.parent = movementHelper.transform;
+
+        cameraDifference = Camera.main.transform.position - transform.position;
+        originalScale = transform.localScale;
+        
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update ()
+    {
+        UpdatePosition();
+        UpdateScale();
+	}
+
+    protected virtual void UpdatePosition()
+    {
+        Vector3 cameraOffset = Camera.main.transform.position + (Camera.main.transform.forward * cameraDifference.magnitude);
+
+        movementHelper.transform.position = cameraOffset;
+        movementHelper.transform.forward = Camera.main.transform.up;
+    }
+    protected virtual void UpdateScale()
+    {
+        float originalDistance = cameraDifference.magnitude;
+        float currentDistance = Vector3.Distance(Camera.main.transform.position, transform.position);
+
+        transform.localScale = Vector3.one * ((currentDistance - originalDistance) / originalDistance) + originalScale;
+    }
+}
